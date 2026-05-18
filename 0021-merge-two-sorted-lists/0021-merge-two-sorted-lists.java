42fd29fe74c55f09bb419550df24fbc9c34a7fa0
Abhijeet Dutta <abhijeetduttaam@gmail.com>
2026-05-18 10:30:45 +0530

    Time: 0 ms (100%), Space: 44.2 MB (73.29%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,26 +8,52 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         ListNode dummy = new ListNode(-1);
+//         ListNode current = dummy;
+
+//         while (list1 != null && list2 != null) {
+//             if (list1.val <= list2.val) {
+//                 current.next = list1;
+//                 list1 = list1.next;
+//             } else {
+//                 current.next = list2;
+//                 list2 = list2.next;
+//             }
+//             current = current.next;
+//         }
+
+//         // Attach the remaining nodes
+//         if (list1 != null) current.next = list1;
+//         if (list2 != null) current.next = list2;
+
+//         return dummy.next;
+//     }
+// }
+class Solution{
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1);
         ListNode current = dummy;
 
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
                 current.next = list1;
                 list1 = list1.next;
-            } else {
+            }
+            else{
                 current.next = list2;
                 list2 = list2.next;
             }
             current = current.next;
         }
 
-        // Attach the remaining nodes
-        if (list1 != null) current.next = list1;
-        if (list2 != null) current.next = list2;
-
-        return dummy.next;
-    }
+        if(list1 != null){
+            current.next = list1;
+        }
+        if(list2 != null){
+            current.next = list2;
+        }
+    return dummy.next;
+    } 
 }
