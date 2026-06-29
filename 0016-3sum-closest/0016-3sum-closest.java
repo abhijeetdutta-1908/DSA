030a63d1ae21f1bc388d20462bc99adb15f63faf
Abhijeet Dutta <abhijeetduttaam@gmail.com>
2026-06-29 12:04:46 +0530

    Time: 19 ms (20.87%), Space: 45.5 MB (48.08%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -4,6 +4,7 @@ class Solution {
         int resultSum = nums[0] + nums[1] + nums[2];
         int minDiff = Integer.MAX_VALUE;
         for(int i = 0 ; i < nums.length -2 ; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int j = i+1;
             int k = nums.length - 1;
             while(j<k){
