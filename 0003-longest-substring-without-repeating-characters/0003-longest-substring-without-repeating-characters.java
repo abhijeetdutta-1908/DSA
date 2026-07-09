72a6a1d96378fd8c907b0171ddfc850d5cba987c
Abhijeet Dutta <abhijeetduttaam@gmail.com>
2026-07-09 21:36:02 +0530

    Time: 5 ms (86.66%), Space: 45.6 MB (69.63%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -6,12 +6,11 @@ class Solution {
         for(int right = 0 ; right < s.length() ; right++){
             char ch = s.charAt(right);
             if(map.containsKey(ch)){
-                left = Math.max(left, map.get(ch) + 1);
-            }
-
+                left = Math.max(left, map.get(ch) + 1 );
+            }                                                                                  
             map.put(ch , right);
             longest = Math.max(longest , right - left + 1);
-        }
+        } 
         return longest;
     }
 }
